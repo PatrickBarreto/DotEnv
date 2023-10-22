0f0b54f3a4603070ccaca75c982214cb8167de08
PatrickBarreto <contato.patrickbarreto@gmail.com>
2023-10-21 23:15:55 -0300

    Feat: dotEnv Builder

---
+++ src/DotEnv.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace DotEnv;
+
+class DotEnv {
+
+    /**
+     * This method is responsable to fill the environment variabels with the .env
+     * 
+     * @param string $envPath
+     * @return bool
+     */
+    public function fill(string $envPath){
+        if(file_exists($envPath)) {
+            $openedFile = fopen($envPath, 'r');
+            
+            while(!feof($openedFile)) {
+                $line = fgets($openedFile);     
+                
+                if(strlen($line) <= 1){
+                    continue;
+                }
+
+                putenv(trim($line));
+            }
+
+            fclose($openedFile);
+            
+            return true;
+        }
+        return false;
+    }
+}
